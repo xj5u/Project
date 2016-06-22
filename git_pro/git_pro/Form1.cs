a7b39a097db8e80e06eaa560c66b2ea2bab888eb
xj5u <2355394542@qq.com>
2016-06-22 15:11:23 +0800

    along

--- git_pro/git_pro/Form1.cs
+++ git_pro/git_pro/Form1.cs
@@ -15,5 +15,10 @@ namespace git_pro
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("成功上传了！");
+        }
     }
 }
